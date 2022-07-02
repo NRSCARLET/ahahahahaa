f6b8c1d8000847bd4f35a2b65baa98789c51c861
NR SCARLET <107818695+NRSCARLET@users.noreply.github.com>
2022-07-02 03:05:44 +0000

    added def and look

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from time import sleep 
-print("WOAHHHHH") ; sleep(1.5)
-print("WE'RE HALF WAY THERE") ; sleep(1.5)
-print("WOAHHHHHHHHH") ; sleep(1.5)
-print("PRAYER ON A CHAIR")
+from time import sleep
+def song_words():
+def song_words2():
+thisdict = {
+    "yes", "y", "Y", "YES", "Yes", "YEs", "YeS"
+}
+song = input("song??")
+if song == thisdict:
 
+else:
+     print("fine no song for you")
+     
+for char in song_words:
+    sleep(0.1)
+    print(char, end="", flush=True )
+for char in song_words2:
+    sleep(0.15)
+    print(char, end="", flush=True)
+song_words(): "WOAHHHH \nWE'RE HALF WAY THERE \n"
+song_words2(): "WOAHHHHHH \nPRAYER ON A CHAIR \n"
