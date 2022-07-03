bcc56c7096a7b943c83c76feb19829867d82b895
NR SCARLET <107818695+NRSCARLET@users.noreply.github.com>
2022-07-03 23:59:57 +0000

    mr fixed gonna change order until work

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from time import sleep
 def song_words():
+    lyrics="WOAHHHH \nWE'RE HALF WAY THERE \n"
+    return lyrics
 def song_words2():
+    lyrics="WOAHHHHHH \nPRAYER ON A CHAIR \n"
+    return lyrics
 thisdict = {
     "yes", "y", "Y", "YES", "Yes", "YEs", "YeS"
 }
 song = input("song??")
-if song == thisdict:
+if song in thisdict:
+    for char in song_words():
+        sleep(0.1)
+        print(char, end="", flush=True )
+    for char in song_words2():
+        sleep(0.15)
+        print(char, end="", flush=True)
+    
+else:    
+    print("fine no song for you")
 
-else:
-     print("fine no song for you")
+
      
-for char in song_words:
-    sleep(0.1)
-    print(char, end="", flush=True )
-for char in song_words2:
-    sleep(0.15)
-    print(char, end="", flush=True)
-song_words(): "WOAHHHH \nWE'RE HALF WAY THERE \n"
-song_words2(): "WOAHHHHHH \nPRAYER ON A CHAIR \n"
