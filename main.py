c1c79f1ce8bd185385f8f195ba584951909b84cb
NR SCARLET <107818695+NRSCARLET@users.noreply.github.com>
2022-09-18 23:47:53 +0000

    Finished code

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 from time import sleep
+
+songchoice = False
+
 def song_words():
     lyrics="WOAHHHH \nWE'RE HALF WAY THERE \n"
     return lyrics
 def song_words2():
     lyrics="WOAHHHHHH \nPRAYER ON A CHAIR \n"
     return lyrics
-thisdict = {
+yesdict = {
     "yes", "y", "Y", "YES", "Yes", "YEs", "YeS"
 }
-song = input("song??")
-if song in thisdict:
-    for char in song_words():
-        sleep(0.1)
-        print(char, end="", flush=True )
-    for char in song_words2():
-        sleep(0.15)
-        print(char, end="", flush=True)
-    
-else:    
-    print("fine no song for you")
-
-
+nodict = {
+    "no", "n", "NO", "No", "nO"
+}
+qdict = {
+    "q", "Q"
+}
+while songchoice == False:
+    song = input("song??")
+    if song in yesdict:
+        for char in song_words():
+            sleep(0.1)
+            print(char, end="", flush=True )
+        for char in song_words2():
+            sleep(0.15)
+            print(char, end="", flush=True)
+            songchoice = True
+    elif song in nodict:
+        print("fine no song for you")
+        songchoice = True
+    elif song in qdict:
+        print("ok monke man")
+        songchoice = True
+    else:
+        print("yes or no (or q)")
+        songchoice = False
      
